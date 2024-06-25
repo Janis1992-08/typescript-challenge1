f499385059aad98976b26050d0b6159de5cdd6b6
Janis Schiller <schillerjanis@gmail.com>
2024-06-25 15:25:25 +0200

    Challenge Step 3

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
+// Step 1
 const age:number = 31
 console.log(age)
 
+//Step 2
 for (let i = 1; i <= age; i++) {
     console.log(i)
 }
+
+//Step 3
+
+let message: string
+
+if (age > 18) {
+    message = "du bist Volljährig"
+} else {
+    message = ("du bist noch Minderjährig")
+}
+console.log(message)
 
